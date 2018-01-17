f9301f72281883e641f21d75c23e8ac702ff24d8
Brian Duggan <bduggan3387@gmail.com>
2018-01-17 13:31:42 -0500

    puts collections into alphabetical order

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,5 +1,5 @@
 class Collection < ActiveRecord::Base
   belongs_to :user
   has_many :game_assignments, dependent: :destroy
-  has_many :games, through: :game_assignments
+  has_many :games, -> { order 'games.title' }, through: :game_assignments
 end
