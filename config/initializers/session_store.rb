a864f42097a27b1aff1f7a12331582422f983c2b
Brian Duggan <bduggan3387@gmail.com>
2018-05-05 11:10:18 -0400

    update rails

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_wonder-o-swan_session'
+Rails.application.config.session_store :cookie_store, key: '_wonder_o_swan_session'
