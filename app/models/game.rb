203d6ab657a4ca5202ef69d82afa31318377e087
Brian Duggan <bduggan3387@gmail.com>
2018-05-23 10:10:30 -0400

    prep for heroku

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,5 @@ class Game < ActiveRecord::Base
   has_many :game_assignments, dependent: :destroy
   has_many :collections, through: :game_assignments
 
-  acts_as_taggable_on :genres
+ # acts_as_taggable_on :genres
 end
